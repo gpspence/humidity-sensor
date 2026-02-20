ec67fe61168184bc6d551b52fd62560a6838bdca
gpspence <105173090+gpspence@users.noreply.github.com>
2026-02-20 13:38:45 +0000

    Remove references to ASG and replace with EC2

---
+++ terraform/modules/ec2/main.tf
@@ -0,0 +1,93 @@
+# --- IAM Permissions/ Roles ---
+resource "aws_iam_role" "ec2" {
+  name               = "ec2-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = ["ec2.amazonaws.com"]
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "ec2-policy"
+  role = aws_iam_role.ec2.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter",
+          "ssm:GetParameters",
+          "ssm:GetParametersByPath"
+        ]
+        Resource = var.secret_arns
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2" {
+  name = "ec2"
+  role = aws_iam_role.ec2.name
+}
+
+
+# --- EC2 ---
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "main" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  iam_instance_profile = aws_iam_instance_profile.ec2.name
+
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  ebs_optimized = true
+  monitoring = true
+
+  disable_api_termination = true
+
+  metadata_options {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  user_data = file("${path.module}/src/cloud-init.sh")
+}
+
+resource "aws_ebs_volume" "main" {
+  availability_zone = aws_instance.main.availability_zone
+  size              = 20
+  type = "gp3"
+}
+
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdf"
+  volume_id   = aws_ebs_volume.main.id
+  instance_id = aws_instance.main.id
+}
---
+++ terraform/modules/ec2/outputs.tf
@@ -0,0 +1,4 @@
+output "main_instance_id" {
+  description = "ID of the main EC2 instance"
+  value = aws_instance.main.id
+}
